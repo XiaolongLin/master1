477c890ff1f67b36876923a80ea3eb6d5e18fae2
Xiaolong Lin <xxl5453@psu.edu>
2020-08-29 04:03:54 +0000

    1

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 C = input("Enter temperature in celsius: ")
 C = int(C)
 F = C*1.8+32
-print( f"{C} in Celsius is equivalent to  {F} Fahrenheit.")
+print( f"{C} in Celsius is equivalent to {F} Fahrenheit.")
